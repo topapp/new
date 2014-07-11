40d57094941c0498220b73f2755eba367c2884dc
LiYX <493234798@qq.com>
2014-07-11 19:49:25 +0800

    servlet接口

---
+++ test/WEB-INF/servlet源码/Diary.java
@@ -0,0 +1,17 @@
+public class Diary{
+    public String url1;
+    public String url2;
+    public String url3;
+    public int picturenum;
+    public String time;
+    public String diary;
+    public Diary(String url1,String url2,String url3,int picturenum,String time,String diary)
+    {
+        this.url1=url1;
+        this.url2=url2;
+        this.url3=url3;
+        this.picturenum=picturenum;
+        this.time=time;
+        this.diary=diary;
+    }
+}
--- test/WEB-INF/servlet源码/DownloadDiary.java
+++ test/WEB-INF/servlet源码/DownloadDiary.java
@@ -13,27 +13,31 @@ import net.sf.json.*;
   
 @WebServlet("/DownloadDiary")
 
-
 public class DownloadDiary extends HttpServlet {  
+
   
   
-    public void doGet(HttpServletRequest request, HttpServletResponse response)  
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {  
         this.doPost(request, response);  
     }  
   
     public void doPost(HttpServletRequest request, HttpServletResponse response)  
             throws ServletException, IOException {  
-        request.setCharacterEncoding("utf-8");   
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");   
         Connection conn=null;
         Statement stmt=null;
         ResultSet rs=null;
         String id=request.getParameter("id");
         int page=Integer.parseInt(request.getParameter("page"));
         int pagesize=Integer.parseInt(request.getParameter("pagesize"));
+        System.out.println(page);
+        System.out.println(pagesize);
+        System.out.println(id);
         JSONObject json=new JSONObject();
-        List list=new ArrayList();
-        String url="http://192.168.23.1:8080/apache-tomcat-7.0.47/webapps/test/clients/";
+        List<Diary> list=new ArrayList<Diary>();
+        String url="http://192.168.23.1:8080/test/clients/";
         try{
             String driverClass="com.mysql.jdbc.Driver";
             String ur1="jdbc:mysql://localhost:3306/davidlee";
@@ -48,35 +52,43 @@ public class DownloadDiary extends HttpServlet {
         catch(SQLException e){
             System.out.println(e.toString());
         }
-        try{                                                                                    //查找匹配账号密码
+        try{                                                                                    //
             while(rs.next())
             {
-                String url1,url2,url3;
+                String url1,url2,url3,diary;
                 int picturenum;
                 Timestamp date=rs.getTimestamp("time");
                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 String strTime = dateFormat.format(date);
-                url1=url+rs.getSring("pic1");
-                url2=url+rs.getSring("pic2");
-                url3=url+rs.getSring("pic3");
+                url1=url+rs.getString("pic1");
+                url2=url+rs.getString("pic2");
+                url3=url+rs.getString("pic3");
                 picturenum=rs.getInt("picturenum");
-                list.add(new Diary(url1,url2,url3,picturenum,strTime));
+                diary=rs.getString("diary");
+                list.add(new Diary(url1,url2,url3,picturenum,strTime,diary));
             }
         }
         catch(SQLException e){
             System.out.println(e.toString());
         }
-
-        for(int i=0;i<pagesize;i++)
+        
+        int i;
+        for(i=0;i<pagesize;i++)
         {
             if(i+(page-1)*pagesize>=list.size())
                 break;
             else
             {
-
+                Diary diary=(Diary)list.get(i+(page-1)*pagesize);
+                json.put("diary"+(i+1)+"url1",diary.url1);
+                json.put("diary"+(i+1)+"url2",diary.url2);
+                json.put("diary"+(i+1)+"url3",diary.url3);
+                json.put("diary"+(i+1),diary.diary);
+                json.put("picturenum"+(i+1),""+diary.picturenum);
+                json.put("time"+(i+1),diary.time);
             }
         }
-
+        json.put("diarynum",""+i);
         response.setContentType("application/json");  
         response.getWriter().write(json.toString());  
         response.getWriter().close();
@@ -94,19 +106,21 @@ public class DownloadDiary extends HttpServlet {
         return;
 
     }  
-}  
-public class Diary{
+} /* 
+class Diary{
     public String url1;
     public String url2;
     public String url3;
     public int picturenum;
     public String time;
-    public void(String url1,String url2,String url3,int picturenum,String time)
+    public String diary;
+    public Diary(String url1,String url2,String url3,int picturenum,String time,String diary)
     {
         this.url1=url1;
         this.url2=url2;
         this.url3=url3;
         this.picturenum=picturenum;
         this.time=time;
+        this.diary=diary;
     }
-}
+}*/
